2fef51c58660014e5f28b8c415d00838e5769608
DuvanG <duvan.gonzalez@correo.usa.edu.co>
2021-01-04 17:41:42 -0500

    send token for contacts

--- lib/provider/contacts_provider.dart
+++ lib/provider/contacts_provider.dart
@@ -2,13 +2,20 @@ import 'package:http/http.dart' as http;
 import 'package:promotoraapp/Model/contacts_model.dart';
 import 'dart:convert' as json;
 import 'package:global_configuration/global_configuration.dart';
+import 'package:promotoraapp/preferences/users_preferences.dart';
 
 class ContactsProvider {
   final String _url = GlobalConfiguration().getValue("api_url") + "/contactos";
+  final _prefs = new UserPreferences();
 
   Future<List<ContactsModel>> getContacts() async {
     try {
-      var response = await http.get(_url);
+      String token = _prefs.token;
+      final response = await http.get(
+        _url,
+        headers: {'Authorization': 'Bearer $token'},
+      );
+      print(_prefs.token);
 
       if (response.statusCode == 200) {
         print(response.body);
