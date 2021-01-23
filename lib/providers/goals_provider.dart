22bd392cba1d88529626a524648444908abdee0d
DuvanG <duvan.gonzalez@correo.usa.edu.co>
2021-01-23 00:40:28 -0500

    remove _prefs

--- lib/providers/goals_provider.dart
+++ lib/providers/goals_provider.dart
@@ -2,11 +2,8 @@ import 'package:http/http.dart' as http;
 import 'package:MiPromotora/models/goals_model.dart';
 import 'dart:convert' as json;
 import 'package:global_configuration/global_configuration.dart';
-import 'package:MiPromotora/preferences/users_preferences.dart';
 
 class GoalsProvider {
-  final _prefs = new UserPreferences();
-
   final String _url = GlobalConfiguration().getValue("api_url") +
       "/reporte-integrado-data?user_code=1585&branch_gct=Total&_sort=integrated_report:DESC&_limit=1";
   Future<GoalsModel> getGoal() async {
