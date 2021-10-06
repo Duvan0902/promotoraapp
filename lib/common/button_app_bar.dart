57233a14eab6637fc9fdd1cacad87a64e289356d
Sergio Torres <sdtorresl@unal.edu.co>
2021-10-05 19:11:29 -0500

    Remove unnecesary imports

--- lib/common/button_app_bar.dart
+++ lib/common/button_app_bar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mi_promotora/main.dart';
 
 class BottomAppBarItem {
   BottomAppBarItem({this.iconData, this.text});
